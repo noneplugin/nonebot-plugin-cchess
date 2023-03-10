35a60a9e07c67fa3f1c9176210d337d4cb50ea2c
meetwq <meetwq@gmail.com>
2023-03-10 19:40:50 +0800

    0.2.2

--- nonebot_plugin_cchess/__init__.py
+++ nonebot_plugin_cchess/__init__.py
@@ -55,7 +55,7 @@ __plugin_meta__ = PluginMetadata(
         "unique_name": "cchess",
         "example": "@小Q 象棋人机lv5\n炮二平五\n结束下棋",
         "author": "meetwq <meetwq@gmail.com>",
-        "version": "0.2.1",
+        "version": "0.2.2",
     },
 )
 
